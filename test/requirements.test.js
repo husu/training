4aba5342a8041c8c570fdd28b5ec2ac8e7ee9353
husu <azhong_husu@hotmail.com>
2016-10-12 10:47:54 +0800

    新增培训需求的测试，同时去掉培训服务的测试

---
+++ test/requirements.test.js
@@ -0,0 +1,20 @@
+/**
+ * Created by husu on 16/10/12.
+ */
+'use strict';
+var AV = require('leanengine');
+AV.Promise._isPromisesAPlusCompliant = false;
+var assert = require('assert');
+
+
+
+const chai = require("chai");
+var expect = chai.expect;
+const chaiHttp = require("chai-http");
+
+chai.use(chaiHttp);
+
+
+describe('测试培训需求的相关接口',function() {
+
+});
